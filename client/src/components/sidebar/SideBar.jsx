e73df2191fcea09a8d2c93d2e6869aa93dab5e5a
preetsimon <77456490+preetsimon@users.noreply.github.com>
2023-01-18 22:25:32 -0800

    sidebar done

--- client/src/components/sidebar/SideBar.jsx
+++ client/src/components/sidebar/SideBar.jsx
@@ -3,9 +3,43 @@ import "./sidebar.css";
 const SideBar = () => {
   return (
     <div className="sidebar">
-        
-    </div>
-  )
-}
+      <div className="sidebarItem">
+        <span className="sidebarTitle">ABOUT ME</span>
+        <img
+          src="https://i.pinimg.com/236x/1e/3f/58/1e3f587572a7a7b20bbf1828595a1786--holiday-party-themes-holiday-gift-guide.jpg"
+          alt=""
+        />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod ab
+          commodi perferendis consectetur laboriosam! Assumenda, atque
+          laboriosam ea maxime pariatur, dolor nulla quisquam fuga.
+        </p>
+      </div>
 
-export default SideBar
+      <div className="sidebarItem">
+        <span className="sidebarTitle">CATEGORIES</span>
+        <ul className="sidebarList">
+          <li className="sidebarListItem">Life</li>
+          <li className="sidebarListItem">Music</li>
+          <li className="sidebarListItem">Style</li>
+          <li className="sidebarListItem">Sports</li>
+          <li className="sidebarListItem">Cinema</li>
+          <li className="sidebarListItem">Tech</li>
+        </ul>
+      </div>
+      
+      <div className="sidebarItem">
+      <span className="sidebarTitle">FOLLOW US</span>
+      <div className="sidebarSocial">
+      <i className="sidebarIcon fab fa-facebook"></i> 
+        <i className="sidebarIcon fa-brands fa-twitter"></i>
+        <i className="sidebarIcon fa-brands fa-pinterest"></i>
+        <i className="sidebarIcon fa-brands fa-square-instagram"></i>
+      </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default SideBar;
