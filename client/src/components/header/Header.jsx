cb6755be3158674efb91bc8bd75b0b404017a79e
preetsimon <77456490+preetsimon@users.noreply.github.com>
2023-02-20 05:52:43 -0800

    css

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
         <span className="headerTitleSm">React & Node</span>
         <span className="headerTitleLg">Blog</span>
       </div>
-      <img className="headerImg" src="https://images.pexels.com/photos/1752806/pexels-photo-1752806.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="headerImg" />
+      {/* <img className="headerImg" src="https://images.pexels.com/photos/1752806/pexels-photo-1752806.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="headerImg" /> */}
     </div>
   )
 }
